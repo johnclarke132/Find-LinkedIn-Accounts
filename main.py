234487fc520f63b316c58ad80bb8050b4d790564
john132 <johnclarke132@gmail.com>
2022-09-19 21:35:49 +0100

    Cleaned up variables for easier user testing

--- main.py
+++ main.py
@@ -9,42 +9,50 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 
+#  Variables
+browser_file_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+search_engine_url = "https:www.google.com"
 
-with open('concatenated search .csv') as csvfile:
-    spamreader = reader(csvfile, delimiter=',')
-    for row in spamreader:
-        # Randomise wait time between clicks
-        time_to_sleep = randint(3, 10)
+accept_cookie_xpath = "//*[@id=\"L2AGLb\"]/div"
+search_bar_xpath = "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"
 
-        # Creating an instance
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        chrome_options = Options()
-        chrome_options.add_argument("--window-size=1100,1000")
-        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        chrome_options.add_experimental_option('useAutomationExtension', False)
-        driver.execute_cdp_cmd('Network.setUserAgentOverride', {
-            "userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
-        driver.get("C:\Program Files\Google\Chrome\Application\chrome.exe")
-        driver.get('https:www.google.com')
-        sleep(time_to_sleep)
+url_class = "MjjYud"
 
-        # Accept cookie policy
-        driver.find_element(By.XPATH, '//*[@id="L2AGLb"]/div').click()
-        sleep(time_to_sleep)
+#  Initialise
+if __name__ == "__main__":
+    with open('concatenated search .csv') as csvfile:
+        spamreader = reader(csvfile, delimiter=',')
+        for row in spamreader:
+            # Randomise wait time between clicks
+            time_to_sleep = randint(3, 10)
 
-        # Search current row in browser
-        search_query = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
-        search_query.send_keys(row)
-        sleep(time_to_sleep)
+            # Creating an instance
+            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+            chrome_options = Options()
+            chrome_options.add_argument("--window-size=1100,1000")
+            chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+            chrome_options.add_experimental_option('useAutomationExtension', False)
+            driver.execute_cdp_cmd('Network.setUserAgentOverride', {
+                "userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+            driver.get(browser_file_location)
+            driver.get(search_engine_url)
+            sleep(time_to_sleep)
 
-        # Query to obtain links
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        search = soup.find_all('div', class_="yuRUbf")
-        for h in search:
-            with open('urls.csv', 'a') as fd:
-                fd.write(h.a.get('href'))
-                fd.write('\n')
+            # Accept cookie policy
+            driver.find_element(By.XPATH, accept_cookie_xpath).click()
+            sleep(time_to_sleep)
 
-        driver.quit()
+            # Search current row in browser
+            search_query = driver.find_element(By.XPATH, search_bar_xpath)
+            search_query.send_keys(row)
+            sleep(time_to_sleep)
 
-        print(row)
+            # Query to obtain links
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            search = soup.find_all('div', class_=url_class)
+            for h in search:
+                with open('urls.csv', 'a') as fd:
+                    fd.write(h.a.get('href'))
+                    fd.write('\n')
+
+            driver.quit()
